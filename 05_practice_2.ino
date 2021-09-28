080a42e2248661b1eeba334d4fa7d8d11f24b7cd
klaqwe1 <66315495+klaqwe1@users.noreply.github.com>
2021-09-28 23:48:05 +0900

    Create 05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,33 @@
+#define PIN_LED 7
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, 0);
+  delay(1000);
+  digitalWrite(PIN_LED, 1);
+  delay(200);
+  digitalWrite(PIN_LED, 0);
+  delay(200);
+  digitalWrite(PIN_LED, 1);
+  delay(200);
+  digitalWrite(PIN_LED, 0);
+  delay(200);
+  digitalWrite(PIN_LED, 1);
+  delay(200);
+  digitalWrite(PIN_LED, 0);
+  delay(200);
+  digitalWrite(PIN_LED, 1);
+  delay(200);
+  digitalWrite(PIN_LED, 0);
+  delay(200);
+  digitalWrite(PIN_LED, 1);
+  delay(200);
+  digitalWrite(PIN_LED, 0);
+  delay(200);
+  digitalWrite(PIN_LED, 1);
+}
+
+void loop() {
+    
+        
+}
